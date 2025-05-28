c6fe6b01fefec37d91349fa990a10845c4460179
joonaskoskinen <149376043+joonaskoskinen@users.noreply.github.com>
2025-05-28 11:35:18 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Konfiguraatioasetukset
+  transpilePackages: ["framer-motion"],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
